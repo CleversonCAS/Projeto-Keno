ad12628c4e022c2661a9fbe7f36414009b7e0bb4
CleversonCAS <42379258+CleversonCAS@users.noreply.github.com>
2018-10-11 20:36:39 -0300

    Add files via upload

--- kenoH.h
+++ kenoH.h
@@ -8,9 +8,11 @@
 
 using number_type = int; //<! data type for a keno hit.
 
+void ordenaVetor(std::vector<number_type> &vet);
+
 void encerraPrograma(float creditoInicial, float creditoFinal);
 
-bool verificaNumero(std::vector<number_type> &testeNum,int tam, number_type valor);//Verifica se o numero selecionado está no vetor
+bool verificaNumero(number_type valor,number_type valor2);//Verifica se o numero selecionado está no vetor
 
 void geraNumSorteados(std::vector<number_type> &vec);
 
@@ -21,6 +23,10 @@ int rand_number();
 void imprimeVetor(std::vector<number_type> &vector);
 
 void imprimePonto();
+
+float creditoAtual(float x, int multi , float apostaAtual);//recebe o credito atual e atualiza de acordo com a tabela
+
+float creditoSoma(float creditoAtual, float creditoDaRodada);	//recebe o credito atual e e o inicial e faz a soma
 
 class KenoBet{
 	private:
@@ -41,14 +47,6 @@ class KenoBet{
 		void setRodadas(number_type x);		//recebe num de rodadas
 		number_type getRodadas();			//devolve num de rodadas
 
-		void setCreditoAtual(float x, int multi);		//recebe o credito atual e atualiza de acordo com a tabela
-		float getCreditoAtual();								//devolve o credito atual
-
-		void setCreditoSoma(float creditoAtual, float credito);		//recebe o credito atual e e o inicial e faz a soma
-		float getCreditoSoma();										//devolve o credito atual
-
-
-
 		void setApostas(std::vector<number_type> &apostas);//Recebe um vetor com as apostas dadas pelo jogador no arquivo //Atualizar para que devolva apenas as apostas uteis
 
 		bool verificaCredito(float apostaDada);//Verifica se a aposta é válida
@@ -59,8 +57,5 @@ class KenoBet{
 
 		
 };
-
-
-
 
 #endif
